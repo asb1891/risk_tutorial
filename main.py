ad7e3b7de7e5a0f195eaed499f3aa712206a4a0a
asb1891 <137960726+asb1891@users.noreply.github.com>
2023-11-08 14:14:21 -0500

    main file

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import pygame as pg
+import pygame as pygame
 import sys
 from src.game import Game
 
 # Initialize the pygame modules
-pg.init()
-pg.mixer.init()
-pg.font.init()
+pygame.init()
+pygame.mixer.init()
+pygame.font.init()
 
 # Define the dimensions of the window
 WIDTH = 1600
 HEIGHT = 1200
-window_size = pg.Vector2(WIDTH, HEIGHT)
+window_size = pygame.Vector2(WIDTH, HEIGHT)
 
 # Create the screen surface
-screen = pg.display.set_mode((WIDTH, HEIGHT))  # window_size should be a tuple here
+screen = pygame.display.set_mode((WIDTH, HEIGHT))  # window_size should be a tuple here
 # Initialize the clock
-clock = pg.time.Clock()
+clock = pygame.time.Clock()
 
 # Set the caption of the window
-pg.display.set_caption("Risk")
+pygame.display.set_caption("Risk")
 
 # Now that everything is initialized, create a Game object
 game = Game(screen, clock, window_size)
@@ -27,5 +27,5 @@ game = Game(screen, clock, window_size)
 game.run()
 
 # Quit the game and exit
-pg.quit()
+pygame.quit()
 sys.exit()
