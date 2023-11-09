8f3b4682c7297245fb833e09f949d9f47b09be65
asb1891 <137960726+asb1891@users.noreply.github.com>
2023-11-09 10:28:32 -0500

    player.py

--- src/player.py
+++ src/player.py
@@ -3,61 +3,92 @@ import random
 from src.geo import World, Country
 from src.dice import Dice
 
-# x_attacker= 50
-# y_attacker= 50
-
-# x_defender= 50
-# y_defender= 100
-
 # Assuming the necessary classes are imported from their respective modules
 class Player:
     def __init__(self, game, name, country: Country, world: World, color: tuple) -> None:
-        self.name = name
-        self.game = game
-        self.country = country
-        self.countries = []
-        self.world = world
-        self.plus_button_pos = (0, 0)  # Replace with actual x and y coordinates for the "+" button
-        self.minus_button_pos = (0, 0)
-        self.color = color
+        self.name = name #players name
+        self.game = game #reference to game instance
+        self.country = country # the starting country for the player 
+        self.countries = [] # list of countries owned by the player
+        self.world = world #reference to world instance
+        self.plus_button_pos = (0, 0)  # coordinates for the + button
+        self.minus_button_pos = (0, 0) # coordinates for the - button
+        self.color = color #color assigned ot the player
         self.mouse_get_pressed= pygame.mouse.get_pressed
-        self.country.color = self.color
-        self.timer = pygame.time.get_ticks()
-        self.controlled_countries = [self.country.name]
-        self.neighbours = self.get_neighbours()
+        self.country.color = self.color #color of the countries
+        self.timer = pygame.time.get_ticks() # pygame timeer to contorl tick rate
+        self.controlled_countries = [self.country.name] #list of countries controlled by player, initialized with countries name
+        self.neighbours = self.get_neighbours() #neighboring countries of the player's controlled countries 
 
+    #update mehtod to handle different phases of the game
     def update(self, phase: str) -> None:
         if phase == "place_units":
             self.place_units()
         elif phase == "attack_country":
             self.attack_country()
 
+    #place units method for placing new units on the player's countries
     def place_units(self) -> None:
-        # from src.game import Game
+        #get a list of countries the player can navigate to 
         navigable_countries = self.get_navigable_countries()
         now = pygame.time.get_ticks()
-        
-        # Draw and interact with plus and minus buttons
+        # loop through the navigable countries and place units on hovered country when clicked
         for navigable_country in navigable_countries:
-            if navigable_country.hovered:
-                # Define positions for plus and minus buttons relative to the country's location
-                plus_button_pos= (navigable_country.center[0] +50, navigable_country.center[1]) # Example position
-                minus_button_pos= (navigable_country.center[0] -50, navigable_country.center[1]) # Example position
-
-                # Check if the plus button is clicked
-                if self.world.draw_button(plus_button_pos) and (now - self.timer > 300):
-                    self.timer = now
-                    navigable_country.units += 1  # Increment units
-                
-                # Check if the minus button is clicked
-                if self.world.draw_button(minus_button_pos) and (now - self.timer > 300):
-                    self.timer = now
-                    navigable_country.units -= 1  # Decrement units
-
-            # Your existing code for unit placement by clicking on the country itself
             if navigable_country.hovered and pygame.mouse.get_pressed()[0] and (now - self.timer > 300):
                 self.timer = now
-                navigable_country.units += 1
+                navigable_country.units += 1 #increase units by 1
+
+    def get_navigable_countries(self) -> list:
+        navigable_countries = []
+        print(f"\n>> CURRENT CONTROLLED COUNTRIES PRE-ITERATION: {self.controlled_countries}")
+        for name_of_country in self.controlled_countries:
+            controlled_country = self.world.countries.get(name_of_country)
+            print(f"\t>> CURRENT NAVIGABLE COUNTRIES PRE-ADDITION OF CONTROLLED COUNTRY: {navigable_countries}")
+            navigable_countries.append(controlled_country)
+            print(f"\t>> CURRENT NAVIGABLE COUNTRIES POST-ADDITION OF CONTROLLED COUNTRY: {navigable_countries}")
+            names_of_neighboring_countries = self.world.get_country_neighbours(name_of_country)
+            print(f"\t>> CURRENT NEIGHBORING COUNTRIES FOR COUNTRY={name_of_country}: {self.world.get_country_neighbours(name_of_country)}")
+            for name_of_neighbor in names_of_neighboring_countries:
+                neighboring_country= self.world.countries.get(name_of_neighbor)
+                navigable_countries.append(neighboring_country)
+
+        now = pg.time.get_ticks()      
+        for navigable_country in navigable_countries:
+            if navigable_country.hovered and pg.mouse.get_pressed()[0] and (now - self.timer > 300):
+                self.timer = now
+                navigable_country.units += 1 #increment code to add armies
+
+
+                ##### COMMENTED OUT CODE 
+    # def place_units(self) -> None:
+    #     # from src.game import Game
+    #     navigable_countries = self.get_navigable_countries()
+    #     now = pygame.time.get_ticks()
+        
+    #     # Draw and interact with plus and minus buttons
+    #     for navigable_country in navigable_countries:
+    #         if navigable_country.hovered:
+    #             # Define positions for plus and minus buttons relative to the country's location
+    #             plus_button_pos= (navigable_country.center[0] +50, navigable_country.center[1]) # Example position
+    #             minus_button_pos= (navigable_country.center[0] -50, navigable_country.center[1]) # Example position
+
+    #             # Check if the plus button is clicked
+    #             if self.world.draw_button(plus_button_pos) and (now - self.timer > 300):
+    #                 self.timer = now
+    #                 navigable_country.units += 1  # Increment units
+                
+    #             # Check if the minus button is clicked
+    #             if self.world.draw_button(minus_button_pos) and (now - self.timer > 300):
+    #                 self.timer = now
+    #                 navigable_country.units -= 1  # Decrement units
+
+    #         # existing code for unit placement by clicking on the country itself
+    #             if navigable_country.hovered and pygame.mouse.get_pressed()[0] and (now - self.timer > 300):
+    #                 self.timer = now
+    #                 navigable_country.units += 1
+
+
+
 
     # Helper function to determine if a button is clicked
     def button_clicked(self, button_pos) -> bool:
@@ -72,6 +103,9 @@ class Player:
     def decrement_units(self, country):
         if country.units > 0:  # Prevent negative units
             country.units -= 1
+
+
+            ### COMMENTED OUT CODE 
     # def place_units(self) -> None:
     #     navigable_countries = self.get_navigable_countries()
     #     now = pygame.time.get_ticks()
@@ -110,6 +144,7 @@ class Player:
         if defending_country: #if a defending country is found, execute attack
             self.execute_attack(attacking_country, defending_country)
     
+    # An attack method using the roll dice method 
     def execute_attack(self, attacking_country, defending_country):
         attacker_dice = Dice.attacker_dice_roll(min(3, attacking_country.units - 1))
         defender_dice = Dice.defender_dice_roll(min(2, defending_country.units))
@@ -136,7 +171,7 @@ class Player:
                       if neighbour not in self.controlled_countries}
         return neighbours
     
-
+        ### COMMENTED OUT CODE
   # def update_dice_display(self, attacker_dice, defender_dice):
     #     font= pygame.font.Font(None, 36)
 
